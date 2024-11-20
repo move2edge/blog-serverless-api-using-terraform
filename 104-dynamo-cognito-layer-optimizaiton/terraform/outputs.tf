8b0a001e9a5baf7b7bfea119ba6b4008c9cb5b67
Patryk Kaczmarek <patryk.kaczmarek@wp.eu>
2024-11-20 12:34:42 +0100

    101

--- 104-dynamo-cognito-layer-optimizaiton/terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-# terraform/outputs.tf
-
-# This Terraform configuration defines the following output:
-# 1. invoke_url: The URL to invoke the API Gateway stage.
-
-output "invoke_url" {
-  value = module.apigateway.invoke_url
-}
